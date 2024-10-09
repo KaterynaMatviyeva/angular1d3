15a8b02569472b09ca93a913514cdbd0eeac02c3
Kateryna <katiaemme96@yahoo.it>
2024-10-09 16:53:29 +0200

    work in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivePostComponent } from './components/active-post/active-post.component';
+import { InactivePostComponent } from './components/inactive-post/inactive-post.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivePostComponent,
+    InactivePostComponent,
+    PostDetailComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
